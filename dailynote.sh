e2a1f3f5f217a5ba4f8733fffb58c5cc04b24195
Kristian Skibb Skibrek <kristianskibrek@protonmail.com>
2023-08-21 14:00:43 +0200

    add daily note script

---
+++ dailynote.sh
@@ -0,0 +1,12 @@
+# script to open the daily note in the obsidian vault
+
+NOTE=$HOME/Documents/Obsidian\ "Vault"/
+DAILY=daily\ "notes"
+
+date=$(date '+%Y-%m-%d')
+
+#running nvim from binary file lol
+editor=/home/kristian/Programs/nvim-linux64/bin/nvim
+
+cd "$NOTE"
+$editor "$DAILY"/"$date".md
