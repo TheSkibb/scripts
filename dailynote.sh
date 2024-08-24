469e70973e6609d7d0a3773f21a7d98ac54ec587
theSkibb <kristianskibrek@protonmail.com>
2024-08-24 10:04:04 +0200

    saner defaults for directories and using editor env variable

--- dailynote.sh
+++ dailynote.sh
@@ -1,15 +1,17 @@
 # script to open the daily note in the obsidian vault
 
-NOTE=$HOME/skolearbeidUiA/matte/semester2/notes
+NOTE=$HOME/Notes
 DAILY_DIR=dailyNotes
 WEEKLY_DIR=weeklyNotes
 
 DAILY_FORMAT='+%Y-%m-%d'
 WEEKLY_FORMAT='+%Y-W%W'
 
-
-#running nvim from binary file lol
-editor=/home/kristian/Programs/nvim-linux64/bin/nvim
+if [[ -z $EDITOR ]] then
+	editor=$EDITOR
+else
+	editor=vim
+fi
 
 cd "$NOTE"
 
