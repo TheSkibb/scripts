8067438be7e4555b893165cc88c6fc30600230cd
theskibb <kristianskibrek@protonmail.com>
2025-03-20 18:02:41 +0100

    use containers specified command

--- podmaninator.sh
+++ podmaninator.sh
@@ -5,7 +5,9 @@ selection=$(podman ps -a --format "{{.ID}} {{.Names}} {{.Image}}" | fzf)
 if [[ -n "$selection" ]]; then
 
     id=$(echo $selection | cut -c -12)
-    podman start $id && podman exec -it $id /bin/bash
+    command=$(podman ps --filter id=$id --format "{{.Command}}")
+    echo $command
+    podman start $id && podman exec -it $id $command
 
 else
     echo "no selection made"
