1e321f73866af47cff82ed50a780d97864971ad6
theskibb <kristianskibrek@protonmail.com>
2025-04-08 10:50:33 +0200

    remove unnused scripts

--- i3lock.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-/usr/bin/i3lock -i /home/kristian/Pictures/wallpapers/space2.png
-#systemctl suspend
--- notifySuspend.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-notify-send -u normal -i none "inactive in fifteen minutes, suspending soon"
