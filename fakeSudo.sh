a54f6165912a3325ad76f6a8b9c92c3a6408fa51
theskibb <kristianskibrek@protonmail.com>
2025-11-02 14:28:54 +0100

    add shebang, description and newline before last error

--- fakeSudo.sh
+++ fakeSudo.sh
@@ -1,3 +1,6 @@
+#! /usr/bin/env bash
+# sudo which will always result in wrong password
+
 read -s -p "[sudo] password for ${USER}: " throwaway
 echo
 sleep 1
@@ -7,4 +10,5 @@ echo
 sleep 1
 echo "Sorry, try again"
 read -s -p "[sudo] password for ${USER}: " throwaway
+echo
 echo "sudo: 3 incorrect password attempts"
